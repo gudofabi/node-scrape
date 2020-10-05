96f3870a17dba5f8b654f6400070ed1ed88dfb4c
Gudo Fabi <godofredo.fabi@growsuper.com>
2020-10-05 21:39:35 +0800

    fix: install xlsx

--- pl-scraper.js
+++ pl-scraper.js
@@ -1,6 +1,14 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 const readline = require("readline");
+const xlsx = require("xlsx");
+
+// https://www.businesslist.ph/company/263811/gigz-superclub
+const wb = xlsx.readFile("nyahaha.csv", {cellDates: true});
+const ws = wb.Sheets["Sheet1"];
+
+const wsData = xlsx.utils.sheet_to_json(ws);
+
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
@@ -16,7 +24,6 @@ rl.on('line', (line) => {
             .then(response => {
                 const html = response.data;
                 const $ = cheerio.load(html);
-                const scrapeData = [];
 
                 const name = $('.cmp_details_in > .info > #company_name').text();
                 const address = $('.cmp_details_in > .info > .location').text();
@@ -26,17 +33,51 @@ rl.on('line', (line) => {
                 const website = $('.cmp_details_in > .info > .weblinks').text();
                 const description = $('.cmp_more > .info > .desc').text();
 
-                scrapeData.push({
-                    name,
-                    address,
-                    city,
-                    state,
-                    phone,
-                    website,
-                    description
+                wsData.push({
+                    'Listing Title': name,
+                    'Listing SEO Title': name,
+                    'Listing Email': '',
+                    'Listing URL': '',
+                    'Listing Address': address,
+                    'Listing Country': 'Philippines',
+                    'Listing Country Abbreviation': 'PHP',
+                    'Listing Region': '',
+                    'Listing Region Abbreviation': '',
+                    'Listing State': state,
+                    'Listing State Abbreviation': '',
+                    'Listing City': city,
+                    'Listing Postal Code': '',
+                    'Listing Phone': phone,
+                    'Listing Short Description': '',
+                    'Listing Long Description': description,
+                    'Listing SEO Description': '',
+                    'Listing Keywords':'',
+                    'Listing Renewal Date': '',
+                    'Listing Status': 'active',
+                    'Listing Level': 'silver',
+                    'Listing Category 1': '',
+                    'Listing Category 2': '',
+                    'Listing Category 3': '',
+                    'Account Username': '',
+                    'Account Password': '',
+                    'Account Contact First Name': '',
+                    'Account Contact Last Name': '',
+                    'Account Contact Company': '',
+                    'Account Contact Address': '',
+                    'Account Contact Country': 'Philippines',
+                    'Account Contact State': state,
+                    'Account Contact City': city,
+                    'Account Contact Postal Code': '',
+                    'Account Contact Phone': phone,
+                    'Account Contact Email': '',
+                    'Account Contact URL': ''
                 });
                 console.log('\n\nScrape Data: ');
-                console.log(scrapeData);
+                const newWS = xlsx.utils.json_to_sheet(wsData);
+                console.log(newWS);
+                xlsx.utils.sheet_to_csv(newWS);
+                const newWB = xlsx.utils.book_append_sheet(wb, newWS, "New Data");
+                xlsx.write(newWB);
                 
             })
             .catch(console.error);
